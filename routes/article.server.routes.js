41d2c088143c7a0395b5e231a73bc4e1cda345d4
Monsura <monsuramayeen1@gmail.com>
2018-03-28 03:18:45 +0000

    assignment 14

--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -1,31 +1,28 @@
 module.exports = function(app){
 
- var products = require('./../controllers/products.server.controller.js');
+ var articles = require('./../controllers/articles.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
- app.route('/api/products')
-	.get(products.list)
-	.post(users.requiresLogin, products.create);
+ app.route('/api/articles')
+	.get(articles.list)
+	.post(users.requiresLogin, articles.create);
 
-  app.route('/api/products/:productId')
-	.get(products.read)
-  .delete(users.requiresLogin, products.delete);
+  app.route('/api/articles/:articleId')
+	.get(articles.read)
+  .delete(users.requiresLogin, articles.delete);
 
-	app.route('/api/products/edit/:productId')
-	.get(products.read)
-	.put(users.requiresLogin, products.update);
+	app.route('/api/articles/edit/:articleId')
+	.get(articles.read)
+	.put(users.requiresLogin, articles.update);
 
-	app.route('/product/:productId')
-	.get(products.view)
-	
-	.get(products.read)
-	.put(users.requiresLogin, products.update);
-	
-app.route('/products/all').get(products.listView);
-app.route('/products/new').get(products.createView);
+app.route('/articles/all').get(articles.listView);
+app.route('/articles/new').get(articles.createView);
+app.route('/article/:articleId').get(articles.singleView);
 
-app.route('/product/:productId').get(products.singleView);
-app.param('productId', products.productByID);
+
+app.param('articleId', articles.articleByID);
+
 
 }
+
 
